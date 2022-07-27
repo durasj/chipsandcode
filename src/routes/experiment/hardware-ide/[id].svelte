67f7e03b033370df83b1a907171b80d18b90bea0
Jakub Duras <jakub@duras.me>
2022-07-27 11:27:39 +0200

    Add experimental svelte endpoints use

--- src/routes/experiment/hardware-ide/[id].svelte
+++ src/routes/experiment/hardware-ide/[id].svelte
@@ -1,34 +1,8 @@
-<script lang="ts" context="module">
-  import type { Load } from '@sveltejs/kit';
-
-  export const load: Load = ({ params }) => ({
-    status: 200,
-    props: { id: params.id },
-  });
-</script>
-
 <script lang="ts">
   import type { Experiment } from 'src/backend/endpoints/experiment';
-  import Problem from 'src/components/Problem.svelte';
   import HardwareIDE from 'src/components/HardwareIDE.svelte';
-  import api from 'src/api';
-  import Loading from 'src/components/Loading.svelte';
 
-  export let id: string;
-
-  $: experiment = (async () => {
-    try {
-      const response = await api<{ experiment: Experiment }>(`/experiment/${id}`);
-
-      return response.experiment;
-    } catch (e) {
-      if (e instanceof Error && e.message) {
-        throw new Error(`Loading failed: ${e.message}`);
-      } else {
-        throw new Error('Unable to load experiment');
-      }
-    }
-  })();
+  export let experiment: Experiment;
 </script>
 
 <svelte:head>
@@ -41,16 +15,6 @@
   {/await}
 </svelte:head>
 
-{#await experiment}
-  <main class="flex flex-grow" aria-live="polite" aria-busy="true">
-    <Loading name="Experiment" />
-  </main>
-{:then experiment}
-  <main class="flex flex-grow" aria-live="polite" aria-busy="false">
-    <HardwareIDE {experiment} />
-  </main>
-{:catch error}
-  <main class="flex flex-grow" aria-live="polite" aria-busy="false">
-    <Problem message={error.message} />
-  </main>
-{/await}
+<main class="flex flex-grow" aria-live="polite" aria-busy="false">
+  <HardwareIDE {experiment} />
+</main>
--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import { mdsvex } from 'mdsvex';
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
 
 import mdsvexConfig from './mdsvex.config.js';
 
