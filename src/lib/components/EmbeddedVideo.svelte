dd02b44e018fb339f865959f913299af87662aae
Jakub Duras <jakub@duras.me>
2023-08-06 01:33:50 +0200

    Disable related videos on youtube embed

--- src/lib/components/EmbeddedVideo.svelte
+++ src/lib/components/EmbeddedVideo.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <iframe
-  src={`https://www.youtube-nocookie.com/embed/${id}`}
+  src={`https://www.youtube-nocookie.com/embed/${id}?rel=0`}
   class="not-prose relative w-full 2xl:w-[260%] 2xl:-left-[80%] xl:w-[220%] xl:-left-[60%] lg:w-[174%] lg:-left-[37%] md:w-[120%] md:-left-[10%] min-h-[40rem] z-10 my-12"
   title="YouTube video player"
   frameborder="0"
